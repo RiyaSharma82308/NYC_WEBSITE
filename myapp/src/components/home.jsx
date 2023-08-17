911342ec5785d077b99166aae619a4e17f3d3962
sahilmotwani17 <smotwani17@gmail.com>
2023-08-17 12:10:23 +0530

    home

---
+++ myapp/src/components/home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div style={{backgroundColor:"rgba(235, 230, 204, 0.8)", height:"56.52vw" }}>
+        <span style={{fontSize:"4.64vw"}}>We are...</span>
+      </div>
+    )
+  }
+}
