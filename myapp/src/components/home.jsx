a459f4d6abdc7d18767336605c77a70a04182ac4
sahilmotwani17 <smotwani17@gmail.com>
2023-08-17 15:04:33 +0530

    home3

--- myapp/src/components/home.jsx
+++ myapp/src/components/home.jsx
@@ -1,11 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import homewhy1 from '../images/homewhy1.png';
+import homewhy2 from '../images/homewhy2.png';
+import homewhy3 from '../images/homewhy3.png';
+
 
 export default class Home extends Component {
   render() {
     return (
-      <div style={{backgroundColor:"rgba(235, 230, 204, 0.8)", height:"56.52vw" }}>
-        <span style={{fontSize:"4.64vw", fontFamily:"Hiragino Sans", fontWeight:'bold'}}>We are...</span>
+      <div>
+        <div
+          style={{
+            backgroundColor: "rgba(235, 230, 204, 0.8)",
+            display: "flex", // Use flexbox for layout
+            flexDirection: "column", // Stack elements vertically
+            alignItems: "flex-start", // Align items to the left
+            height: "56.52vw",
+            paddingLeft: "5%", // Add left padding for the container
+            marginBottom: "2rem", // Add margin at the bottom to create space
+          }}
+        >
+          <span
+            style={{
+              fontSize: "4.64vw",
+              fontFamily: "Hiragino Sans",
+              fontWeight: "bold",
+              textAlign: "left", // Align text to the left
+              maxWidth: "90%", // Limit the width of the text to maintain responsiveness
+              marginTop: "11vw",
+            }}
+          >
+            We are...
+          </span>
+          <span
+            style={{
+              fontFamily: "Zabal DEMO",
+              fontSize: "1.74vw",
+              fontWeight: "500", // Adjusted font weight
+              textAlign: "left", // Align text to the left
+              marginTop: "1rem", // Add some top margin to separate the spans
+            }}
+          >
+            Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit.<br /><br />{" "}
+            <span style={{ fontWeight: "900" }}>Dolor sit amet.</span>
+          </span>
+        </div>
+
+        <div style={{ marginBottom: "3rem", marginLeft:"3vw" }}> {/* Add margin at the bottom to move the next section down */}
+          <span style={{ fontFamily: "Hiragino Sans", fontSize: "2.32vw", fontWeight: "800" }}>Why we exist?</span>
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", paddingLeft:"6vw", paddingRight:"6vw",fontSize:"1.3vw", fontFamily:"Zabal DEMO", fontWeight:"500" }}>
+          <div style={{ textAlign: "center"}}>
+            <img src={homewhy1} alt="Image 1" style={{ width: "17.39vw", height: "17.39vw" }} />
+            <p>To revolutionise the way <br/> businesses operate</p>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <img src={homewhy2} alt="Image 2" style={{ width: "17.39vw", height: "17.39vw" }} />
+            <p>To enable seamless growth<br/> and optimisation of resources</p>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <img src={homewhy3} alt="Image 3" style={{ width: "17.39vw", height: "17.39vw" }} />
+            <p>Lorem ipsum dolor sit amet,<br/> consectetur adipiscing elit.</p>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
