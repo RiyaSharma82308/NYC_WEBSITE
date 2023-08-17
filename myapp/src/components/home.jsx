178c8b44ebb57e27a8a775eb22bb85e43a20d91d
sahilmotwani17 <smotwani17@gmail.com>
2023-08-17 15:51:04 +0530

    home4

--- myapp/src/components/home.jsx
+++ myapp/src/components/home.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import homewhy1 from '../images/homewhy1.png';
 import homewhy2 from '../images/homewhy2.png';
 import homewhy3 from '../images/homewhy3.png';
+import homescaling from '../images/homescaling.png';
+import homeestate from '../images/homeestate.png';
+import hometea from '../images/hometea.png';
+import homerunway from '../images/homerunway.png';
+import homestrips from '../images/homestrips.png';
 
 
 export default class Home extends Component {
@@ -16,7 +21,7 @@ export default class Home extends Component {
             alignItems: "flex-start", // Align items to the left
             height: "56.52vw",
             paddingLeft: "5%", // Add left padding for the container
-            marginBottom: "2rem", // Add margin at the bottom to create space
+            marginBottom: "2vw", // Add margin at the bottom to create space
           }}
         >
           <span
@@ -37,7 +42,7 @@ export default class Home extends Component {
               fontSize: "1.74vw",
               fontWeight: "500", // Adjusted font weight
               textAlign: "left", // Align text to the left
-              marginTop: "1rem", // Add some top margin to separate the spans
+              marginTop: "1vw", // Add some top margin to separate the spans
             }}
           >
             Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit.<br /><br />{" "}
@@ -45,7 +50,7 @@ export default class Home extends Component {
           </span>
         </div>
 
-        <div style={{ marginBottom: "3rem", marginLeft:"3vw" }}> {/* Add margin at the bottom to move the next section down */}
+        <div style={{ marginBottom: "4vw",marginTop:"4vw", marginLeft:"3vw" }}> {/* Add margin at the bottom to move the next section down */}
           <span style={{ fontFamily: "Hiragino Sans", fontSize: "2.32vw", fontWeight: "800" }}>Why we exist?</span>
         </div>
 
@@ -62,7 +67,39 @@ export default class Home extends Component {
             <img src={homewhy3} alt="Image 3" style={{ width: "17.39vw", height: "17.39vw" }} />
             <p>Lorem ipsum dolor sit amet,<br/> consectetur adipiscing elit.</p>
           </div>
+          </div>
+          <div style={{  display: "flex", justifyContent: "center", marginTop:"6vw" }}>
+          <a
+            href="#"
+            style={{
+              display: "inline-block",
+              padding: "0.72vw 1.45vw",
+              backgroundColor: "white",
+              border: "0.07vw solid black",
+              color: "black",
+              textDecoration: "none",
+              fontSize: "1.3vw",
+              marginBottom:"7vw"
+            }}
+          >
+            Discover our subsidiaries
+          </a>
         </div>
+      <div style={{display: "flex", justifyContent: "center"}}>
+      <img style={{width:"97.3vw", height:"50.72vw"}} src={homescaling} alt="Image 2" />
+      </div>
+      <div style={{display: "flex", justifyContent: "center", marginTop:"3.3vw"}}>
+      <img style={{width:"97.3vw", height:"50.72vw"}} src={homeestate} alt="Image 2" />
+      </div>
+      <div style={{display: "flex", justifyContent: "center", marginTop:"3.3vw"}}>
+      <img style={{width:"97.3vw", height:"50.72vw"}} src={hometea} alt="Image 2" />
+      </div>
+      <div style={{display: "flex", justifyContent: "center", marginTop:"3.3vw"}}>
+      <img style={{width:"97.3vw", height:"50.72vw"}} src={homerunway} alt="Image 2" />
+      </div>
+      <div style={{display: "flex", justifyContent: "center", marginTop:"3.3vw"}}>
+      <img style={{width:"97.3vw", height:"50.72vw"}} src={homestrips} alt="Image 2" />
+      </div>
       </div>
     );
   }
